819bf68c9f6261a2cd3807137030181ee9c98919
juliaacampioto <julia.campioto@sptech.school>
2023-11-16 00:49:55 -0300

    Arrumando commits

--- cadastro.js
+++
@@ -1,5 +0,0 @@
-// let deg = 6;
-// for (let i = 1; i <= 20; i++) {
-//   let div = document.querySelector(".cadastre-se div:nth-child(" + i + ")");
-//   div.style.transform = 'rotate(' + deg + 'deg)';
-  
