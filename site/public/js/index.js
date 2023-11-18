7d43c72628c488d53a7f5d8f157dfe492be21fff
juliaacampioto <julia.campioto@sptech.school>
2023-11-18 09:52:08 -0300

    Fazendo o sobre mim

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -1,227 +1,219 @@
-// Romance
-
-function abrirModal(){
+function abrirModal() {
     const modal = document.getElementById('div_livro_detalhado')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar' || e.target.id == 'div_livro_detalhado'){
+        if (e.target.id == 'fechar' || e.target.id == 'div_livro_detalhado') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-function abrirModal2(){
+function abrirModal2() {
     const modal = document.getElementById('div_livro_detalhado2')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar2' || e.target.id == 'div_livro_detalhado2'){
+        if (e.target.id == 'fechar2' || e.target.id == 'div_livro_detalhado2') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-function abrirModal3(){
+function abrirModal3() {
     const modal = document.getElementById('div_livro_detalhado3')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar3' || e.target.id == 'div_livro_detalhado3'){
+        if (e.target.id == 'fechar3' || e.target.id == 'div_livro_detalhado3') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-function abrirModal4(){
+function abrirModal4() {
     const modal = document.getElementById('div_livro_detalhado4')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar4' || e.target.id == 'div_livro_detalhado4'){
+        if (e.target.id == 'fechar4' || e.target.id == 'div_livro_detalhado4') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-function abrirModal5(){
+function abrirModal5() {
     const modal = document.getElementById('div_livro_detalhado5')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar5' || e.target.id == 'div_livro_detalhado5'){
+        if (e.target.id == 'fechar5' || e.target.id == 'div_livro_detalhado5') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-// Ficção
-
-function abrirModal6(){
+function abrirModal6() {
     const modal = document.getElementById('div_livro_detalhado6')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar6' || e.target.id == 'div_livro_detalhado6'){
+        if (e.target.id == 'fechar6' || e.target.id == 'div_livro_detalhado6') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-function abrirModal7(){
+function abrirModal7() {
     const modal = document.getElementById('div_livro_detalhado7')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar7' || e.target.id == 'div_livro_detalhado7'){
+        if (e.target.id == 'fechar7' || e.target.id == 'div_livro_detalhado7') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-function abrirModal8(){
+function abrirModal8() {
     const modal = document.getElementById('div_livro_detalhado8')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar8' || e.target.id == 'div_livro_detalhado8'){
+        if (e.target.id == 'fechar8' || e.target.id == 'div_livro_detalhado8') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-function abrirModal9(){
+function abrirModal9() {
     const modal = document.getElementById('div_livro_detalhado9')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar9' || e.target.id == 'div_livro_detalhado9'){
+        if (e.target.id == 'fechar9' || e.target.id == 'div_livro_detalhado9') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-function abrirModal10(){
+function abrirModal10() {
     const modal = document.getElementById('div_livro_detalhado10')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar10' || e.target.id == 'div_livro_detalhado10'){
+        if (e.target.id == 'fechar10' || e.target.id == 'div_livro_detalhado10') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-// Ação
-
-function abrirModal11(){
+function abrirModal11() {
     const modal = document.getElementById('div_livro_detalhado11')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar11' || e.target.id == 'div_livro_detalhado11'){
+        if (e.target.id == 'fechar11' || e.target.id == 'div_livro_detalhado11') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-function abrirModal12(){
+function abrirModal12() {
     const modal = document.getElementById('div_livro_detalhado12')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar12' || e.target.id == 'div_livro_detalhado12'){
+        if (e.target.id == 'fechar12' || e.target.id == 'div_livro_detalhado12') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-function abrirModal13(){
+function abrirModal13() {
     const modal = document.getElementById('div_livro_detalhado13')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar13' || e.target.id == 'div_livro_detalhado13'){
+        if (e.target.id == 'fechar13' || e.target.id == 'div_livro_detalhado13') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-function abrirModal14(){
+function abrirModal14() {
     const modal = document.getElementById('div_livro_detalhado14')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar14' || e.target.id == 'div_livro_detalhado14'){
+        if (e.target.id == 'fechar14' || e.target.id == 'div_livro_detalhado14') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-function abrirModal15(){
+function abrirModal15() {
     const modal = document.getElementById('div_livro_detalhado15')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar15' || e.target.id == 'div_livro_detalhado15'){
+        if (e.target.id == 'fechar15' || e.target.id == 'div_livro_detalhado15') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-//  Poesia
-
-function abrirModal16(){
+function abrirModal16() {
     const modal = document.getElementById('div_livro_detalhado16')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar16' || e.target.id == 'div_livro_detalhado16'){
+        if (e.target.id == 'fechar16' || e.target.id == 'div_livro_detalhado16') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-function abrirModal17(){
+function abrirModal17() {
     const modal = document.getElementById('div_livro_detalhado17')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar17' || e.target.id == 'div_livro_detalhado17'){
+        if (e.target.id == 'fechar17' || e.target.id == 'div_livro_detalhado17') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-function abrirModal18(){
+function abrirModal18() {
     const modal = document.getElementById('div_livro_detalhado18')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar18' || e.target.id == 'div_livro_detalhado18'){
+        if (e.target.id == 'fechar18' || e.target.id == 'div_livro_detalhado18') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-function abrirModal19(){
+function abrirModal19() {
     const modal = document.getElementById('div_livro_detalhado19')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar19' || e.target.id == 'div_livro_detalhado19'){
+        if (e.target.id == 'fechar19' || e.target.id == 'div_livro_detalhado19') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
 
-function abrirModal20(){
+function abrirModal20() {
     const modal = document.getElementById('div_livro_detalhado20')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar20' || e.target.id == 'div_livro_detalhado20'){
+        if (e.target.id == 'fechar20' || e.target.id == 'div_livro_detalhado20') {
             modal.classList.remove('abrir')
         }
-    } ) 
+    })
 }
