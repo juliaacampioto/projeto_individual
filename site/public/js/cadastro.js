cd1b4ddfc1072937b223f3a705d0f22a67f2535f
juliaacampioto <julia.campioto@sptech.school>
2023-11-21 23:18:46 -0300

    Fazendo a métrica e o gráfico na dashboard.

--- site/public/js/cadastro.js
+++ site/public/js/cadastro.js
@@ -4,44 +4,44 @@ function validarEmail(emailVar) {
 }
 
 function cadastrar() {
-  
-    var nomeVar = input_nome.value;
-    var sobrenomeVar = input_sobrenome.value;
-    var emailVar = input_email.value;
-    var senhaVar = input_senha.value;
-    var confirmacaoSenhaVar = input_confirmarSenha.value;
 
-    if(nomeVar === '' || sobrenomeVar === '' || emailVar === '' || senhaVar === '' || confirmacaoSenhaVar === '') {
-      div_erro.innerHTML = `Preencha todos os campos!`;
+  var nomeVar = input_nome.value;
+  var sobrenomeVar = input_sobrenome.value;
+  var emailVar = input_email.value;
+  var senhaVar = input_senha.value;
+  var confirmacaoSenhaVar = input_confirmarSenha.value;
+
+  if (nomeVar === '' || sobrenomeVar === '' || emailVar === '' || senhaVar === '' || confirmacaoSenhaVar === '') {
+    div_erro.innerHTML = `Preencha todos os campos!`;
   } else if (senhaVar !== confirmacaoSenhaVar) {
     div_erro.innerHTML = `As senhas não coincidem`;
-}
-  else if(!validarEmail(emailVar)) {
+  }
+  else if (!validarEmail(emailVar)) {
     div_erro.innerHTML = `Por favor, insira um e-mail válido`;
 
-  }  else {
+  } else {
     window.location.href = 'login.html';
-      fetch("/cadastro/cadastrar", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          nomeServer: nomeVar,
-          sobrenomeServer: sobrenomeVar,
-          emailServer: emailVar,
-          senhaServer: senhaVar
-        }),
+    fetch("/cadastro/cadastrar", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nomeServer: nomeVar,
+        sobrenomeServer: sobrenomeVar,
+        emailServer: emailVar,
+        senhaServer: senhaVar
+      }),
+    })
+      .then(function (resposta) {
+        console.log("resposta: ", resposta);
       })
-        .then(function (resposta) {
-          console.log("resposta: ", resposta);
-        })
-        .catch(function (resposta) {
-          console.log(`#ERRO: ${resposta}`);
-        });
+      .catch(function (resposta) {
+        console.log(`#ERRO: ${resposta}`);
+      });
 
-      return false;
+    return false;
 
   }
 
-  }
+}
---
+++ site/public/js/dashboard.js
@@ -0,0 +1,33 @@
+const labels2 = [
+    '2018', 
+    '2019',
+    '2020',
+    '2021',
+    '2022',
+    '2023',
+
+];
+
+const data2 = {
+    labels: labels2,
+    datasets: [
+    {
+        label: 'Quantidade de livros',
+        backgroundColor: 'rgb	(221,160,221) ',
+        borderColor: 'rgb	(221,160,221) ',
+        data: [15, 5, 20, 10, 8, 1],
+    }
+    ]
+};
+
+
+const config2 = {
+    type: 'bar',
+    data: data2,
+    options: {}
+};
+
+const myChart2 = new Chart(
+    document.getElementById('myChart2'),
+    config2
+);
